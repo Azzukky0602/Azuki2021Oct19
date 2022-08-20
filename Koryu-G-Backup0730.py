74e85f77d3b62a8c44a703d9c04b102db68617f6
Azzukky <tsurikichiazuki@yahoo.co.jp>
2022-08-20 23:10:07 +0900

    Update Koryu-G-Backup0730.py

--- Koryu-G-Backup0730.py
+++ Koryu-G-Backup0730.py
@@ -115,6 +115,7 @@ if push == True:
         rd = re.findall(r'\d+', url)[0][:8]
         racedate = dt.strptime(rd, '%Y%m%d')
         racename = soup.find('h2').text.replace('\u3000', ' ')
+        racenumber = soup.find('h1').text[:-4]        
         course_distance = soup.select('#raceInfomation > div > div.raceNote > ul.trackState.trackMainState > li.distance')[0].text.strip()
         if ',' in course_distance:
             course_distance = course_distance.replace(',' , '')
@@ -987,7 +988,7 @@ if push == True:
     hyo3 = hyo2.sort_values('順位')
     hyo3.set_index("順位", inplace=True)
     
-    st.write(racename)
+    st.write('【' + racenumber + '】' +  racename)
     st.table(hyo3)
     
 else:
