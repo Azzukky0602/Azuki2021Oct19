5885a691975d0fb34377be3b236e13a9ada7c537
Azzukky <tsurikichiazuki@yahoo.co.jp>
2022-10-10 19:37:05 +0900

    Update Nankan-WEB20221005.py

--- Nankan-WEB20221005.py
+++ Nankan-WEB20221005.py
@@ -20,7 +20,7 @@ import numpy as np
 
 
 st.title('テキトー指数研究所＠WEB')
-st.header('南関東　永久版')
+st.header('南関東版')
 
 st.write('   ')
 st.info('【参考】テキトー指数の使い方  \nhttps://note.com/tekito_lab/n/n3342d6531772')
