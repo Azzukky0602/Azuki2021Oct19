6f98cd86a22202d37a6b1b77ff63dcf648068ebe
Azzukky <tsurikichiazuki@yahoo.co.jp>
2022-11-20 18:55:00 +0900

    Update TestJ.py

--- TestJ.py
+++ TestJ.py
@@ -739,7 +739,7 @@ if push == True:
 
 
 
-            ts = ((kijun1 *a + kijun2 * b + kijun3 * c + kijun4 * d) + f) * e * g * h * i
+            ts = ((kijun1 * a * 1.5 + kijun2 * b * 1.3 + kijun3 * c *1.1 + kijun4 * d) + f) * e * g * h * i
             tekito_shisu = int(ts)
             tekito_shisu_list.append(tekito_shisu)
 
