17ec23ce47f702363f172a0360977bf83791e71f
Azzukky <tsurikichiazuki@yahoo.co.jp>
2023-05-27 07:44:08 +0900

    Update TestN.py

--- TestN.py
+++ TestN.py
@@ -371,24 +371,24 @@ if push == True:
                 #斤量補正
                 if 800 <= race_distance <= 1000:
                     if '牝' in horse_id:
-                        f = kijun1 * 0.30 * ((float(max(weight_list))) - 2.0 - float(horse_id[10:15].strip()))
-                    else:
-                        f = kijun1 * 0.30 * ((float(max(weight_list))) - float(horse_id[10:15].strip()))
-                elif 1000 < race_distance <= 1400:
-                    if '牝' in horse_id:
-                        f = kijun1 * 0.20 * ((float(max(weight_list))) - 2.0 - float(horse_id[10:15].strip()))
-                    else:
-                        f = kijun1 * 0.20 * ((float(max(weight_list))) - float(horse_id[10:15].strip()))
-                elif 1400 < race_distance <= 1800:
-                    if '牝' in horse_id:
-                        f = kijun1 * 0.10 * ((float(max(weight_list))) - 2.0 - float(horse_id[10:15].strip()))
-                    else:
-                        f = kijun1 * 0.10 * ((float(max(weight_list))) - float(horse_id[10:15].strip()))
-                elif race_distance > 1800:
-                    if '牝' in horse_id:
                         f = kijun1 * 0.15 * ((float(max(weight_list))) - 2.0 - float(horse_id[10:15].strip()))
                     else:
                         f = kijun1 * 0.15 * ((float(max(weight_list))) - float(horse_id[10:15].strip()))
+                elif 1000 < race_distance <= 1400:
+                    if '牝' in horse_id:
+                        f = kijun1 * 0.1166 * ((float(max(weight_list))) - 2.0 - float(horse_id[10:15].strip()))
+                    else:
+                        f = kijun1 * 0.1166 * ((float(max(weight_list))) - float(horse_id[10:15].strip()))
+                elif 1400 < race_distance <= 1800:
+                    if '牝' in horse_id:
+                        f = kijun1 * 0.0833 * ((float(max(weight_list))) - 2.0 - float(horse_id[10:15].strip()))
+                    else:
+                        f = kijun1 * 0.0833 * ((float(max(weight_list))) - float(horse_id[10:15].strip()))
+                elif race_distance > 1800:
+                    if '牝' in horse_id:
+                        f = kijun1 * 0.05 * ((float(max(weight_list))) - 2.0 - float(horse_id[10:15].strip()))
+                    else:
+                        f = kijun1 * 0.05 * ((float(max(weight_list))) - float(horse_id[10:15].strip()))
 
 
                 #休養係数        
@@ -760,24 +760,24 @@ if push == True:
                 #斤量補正        
                 if 800 <= race_distance <= 1000:
                     if '牝' in horse_id:
-                        f = kijun1 * 0.30 * ((float(max(weight_list))) - 2.0 - float(horse_id[10:15].strip()))
-                    else:
-                        f = kijun1 * 0.30 * ((float(max(weight_list))) - float(horse_id[10:15].strip()))
-                elif 1000 < race_distance <= 1400:
-                    if '牝' in horse_id:
-                        f = kijun1 * 0.20 * ((float(max(weight_list))) - 2.0 - float(horse_id[10:15].strip()))
-                    else:
-                        f = kijun1 * 0.20 * ((float(max(weight_list))) - float(horse_id[10:15].strip()))
-                elif 1400 < race_distance <= 1800:
-                    if '牝' in horse_id:
-                        f = kijun1 * 0.10 * ((float(max(weight_list))) - 2.0 - float(horse_id[10:15].strip()))
-                    else:
-                        f = kijun1 * 0.10 * ((float(max(weight_list))) - float(horse_id[10:15].strip()))
-                elif race_distance > 1800:
-                    if '牝' in horse_id:
                         f = kijun1 * 0.15 * ((float(max(weight_list))) - 2.0 - float(horse_id[10:15].strip()))
                     else:
                         f = kijun1 * 0.15 * ((float(max(weight_list))) - float(horse_id[10:15].strip()))
+                elif 1000 < race_distance <= 1400:
+                    if '牝' in horse_id:
+                        f = kijun1 * 0.1166 * ((float(max(weight_list))) - 2.0 - float(horse_id[10:15].strip()))
+                    else:
+                        f = kijun1 * 0.1166 * ((float(max(weight_list))) - float(horse_id[10:15].strip()))
+                elif 1400 < race_distance <= 1800:
+                    if '牝' in horse_id:
+                        f = kijun1 * 0.0833 * ((float(max(weight_list))) - 2.0 - float(horse_id[10:15].strip()))
+                    else:
+                        f = kijun1 * 0.0833 * ((float(max(weight_list))) - float(horse_id[10:15].strip()))
+                elif race_distance > 1800:
+                    if '牝' in horse_id:
+                        f = kijun1 * 0.05 * ((float(max(weight_list))) - 2.0 - float(horse_id[10:15].strip()))
+                    else:
+                        f = kijun1 * 0.05 * ((float(max(weight_list))) - float(horse_id[10:15].strip()))
 
 
                 #休養係数        
