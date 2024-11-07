3c9da78b5eb360001bc43161f6f84738054734c8
Azzukky <tsurikichiazuki@yahoo.co.jp>
2024-11-07 20:08:32 +0900

    Update TestN-Open.py

--- TestN-Open.py
+++ TestN-Open.py
@@ -225,7 +225,7 @@ if push == True:
 
         tekito_shisu = {}
         for horse_id, p_df in processed_horse_results.items():  #p_dfはprocessed dafa frame
-            if len(p_df) < 3:
+            if 0 <= len(p_df) < 3:
                 tekito_shisu[horse_id] = 0
 
             elif len(p_df) == 3:
