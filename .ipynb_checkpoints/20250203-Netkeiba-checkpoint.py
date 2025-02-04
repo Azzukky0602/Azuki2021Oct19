552041cf3bf4c1d67960303f10e1765951245a0a
Azzukky <tsurikichiazuki@yahoo.co.jp>
2025-02-04 21:40:31 +0900

    Update 20250203-Netkeiba-checkpoint.py

--- .ipynb_checkpoints/20250203-Netkeiba-checkpoint.py
+++ .ipynb_checkpoints/20250203-Netkeiba-checkpoint.py
@@ -1225,12 +1225,10 @@ if push == True:
     
     deviation = []
     for i in final_shisu:
-      deviation_value = round((float(i - ave) / std * 10 + 50), 1)
+      deviation_value = round((float(i - ave) / std * 10 + 50))
       deviation.append(deviation_value)
     
     hyo2['deviation'] = deviation
-    hyo2['deviation'] = hyo2['deviation'].map(lambda x: f"{x:.1f}")
-    hyo2['deviation'] = [float(x) for x in hyo2['deviation']]
     
     hyo2['rank'] = hyo2['TS'].rank(ascending=False).astype(int)    
     hyo3 = hyo2[['rank','umaban','name','TS','deviation','jockey','stable', 'gender', 'age', 'starion','owner']]
